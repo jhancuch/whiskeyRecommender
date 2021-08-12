47d72f6d04848c9e80488f26055c0d230af21999
Joe Hancuch <80708639+jhancuch@users.noreply.github.com>
2021-08-12 11:18:13 -0400

    Update whiskeyconsensus.com url scrape - July 2021.py

--- whiskeyconsensus.com url scrape - July 2021.py
+++ whiskeyconsensus.com url scrape - July 2021.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 # load libraries
-
 import os
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
 
-# In[2]:
-
-
 # grab the webpage that contains all of the URLs for the review pages
-
 url = 'https://whiskeyconsensus.com/reviews/'
 
 header = {
@@ -28,13 +21,8 @@ page = requests.get(url, headers=header)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 
-# In[3]:
-
-
 # parse the webpage to extract the url for each review
-
 results = soup.find_all('div', class_ = 'wppr-post-title wppr-col')
-
 
 urlList = []
 
@@ -44,23 +32,6 @@ for urls in results:
     
 datUrl = pd.DataFrame(urlList, columns =['reviewUrl'])
 
-print(datUrl)
 
-
-# In[4]:
-
-
-os.getcwd()
-
-
-# In[5]:
-
-
+# Export data
 datUrl.to_pickle("./whiskeyconsensus-reviews-urls.pkl")
-
-
-# In[ ]:
-
-
-
-
